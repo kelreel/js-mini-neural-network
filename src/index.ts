694eb3eef0e6fb3018de9a774ba3dc86b058e7aa
Kelreel <derodroll@gmail.com>
2020-11-19 20:54:00 +0300

    back propagation error

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,18 @@ type ActivateFunction = (val: number) => number;
 
 class NeuralNetwork {
   activateFunction: Function;
-  constructor(neurons: Neuron[], ridges: Ridge[], activate: ActivateFunction) {
+  speed: number;
+
+  constructor(
+    neurons: Neuron[],
+    ridges: Ridge[],
+    activate: ActivateFunction,
+    speed: number
+  ) {
     this.neurons = neurons;
     this.ridges = ridges;
     this.activateFunction = activate;
+    this.speed = speed;
   }
   // input: number[] = [];
   ridges: Ridge[];
@@ -15,6 +23,9 @@ class NeuralNetwork {
 
   findInRidgesForNeuron = (id: number) =>
     this.ridges.filter((r) => r.outNeuron.id === id);
+
+  findOutRidgesFromNeuron = (id: number) =>
+    this.ridges.filter((r) => r.inNeuron?.id === id);
 
   fit(input_values: number[]) {
     this.ridges.forEach((ridge, index) => {
@@ -42,9 +53,54 @@ class NeuralNetwork {
       n._value = this.activateFunction(neurVal);
     }
 
-    console.log(this.neurons);
-
     return this.neurons[this.neurons.length - 1]._value;
+  }
+
+  backPropagation(input_val: number[], right_val: number) {
+    const calculateLastNeuronError = (n: Neuron): number =>
+      n._value >= 0 ? -(right_val - n._value) : 0;
+
+    this.neurons = this.neurons.reverse();
+
+    this.neurons.forEach((n, index) => {
+      if (index === 0) {
+        this.neurons[0].error = calculateLastNeuronError(this.neurons[0]);
+        return;
+      }
+
+      if (n._value <= 0) {
+        this.neurons[index].error = 0;
+      } else {
+        let ridges = this.findOutRidgesFromNeuron(n.id);
+
+        let err = 0;
+        ridges.forEach((r) => {
+          err += r.weight * this.getNeuronById(r.outNeuron.id).error;
+        });
+
+        this.neurons[index].error = err;
+      }
+    });
+
+    this.neurons = this.neurons.reverse();
+
+    this.ridges = this.ridges.map((ridge, index) => {
+      let rightError = this.getNeuronById(ridge.outNeuron.id).error;
+      let leftVal: number;
+
+      if (ridge.isInput) {
+        leftVal = input_val[ridge.inId];
+      } else if (ridge.bias) {
+        leftVal = 1;
+      } else {
+        leftVal = this.getNeuronById(ridge.inNeuron.id)._value;
+      }
+
+      return {
+        ...ridge,
+        weight: ridge.weight + -this.speed * rightError * leftVal,
+      };
+    });
   }
 }
 
@@ -52,6 +108,7 @@ class Neuron {
   id: number;
   inputLayer: boolean;
   _value: number;
+  error: number | null = null;
 
   constructor(id: number, inputLayer: boolean = false) {
     this.id = id;
@@ -119,8 +176,15 @@ function init() {
     return val > 0 ? val : 0;
   };
 
-  let n = new NeuralNetwork(neurons, ridges, activateF);
-  console.log(n.fit([0.5, 0.9]));
+  let n = new NeuralNetwork(neurons, ridges, activateF, 0.2);
+
+  for (let i = 0; i < 10; i++) {
+    console.log(n.fit([0.1, 0.9]));
+    n.backPropagation([0.1, 0.9], 1);
+
+  }
+  console.log(n.neurons);
+  console.log(n.ridges);
 }
 
 init();
